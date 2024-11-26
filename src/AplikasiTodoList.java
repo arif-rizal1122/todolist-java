1ab8c2edd9518e4571459c39b640fe9be4c5a347
arif-rizal1122 <rizallarif32@gmail.com>
2024-11-26 08:32:05 +0800

    menentukan bisnis logic

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,26 @@
+
+
+
+
+public class AplikasiTodoList {
+
+    public static String[] model = new String[10];
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void showTodoList(){
+
+    }
+    public static void addTodoList() {
+
+    }
+    public static void removeTodoList(){
+
+    }
+
+
+
+}
