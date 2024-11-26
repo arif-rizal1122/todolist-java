e55ad0e08a15410e29ee71f07d93789ad205f7ce
arif-rizal1122 <rizallarif32@gmail.com>
2024-11-26 08:42:58 +0800

    impl kode untuk show todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -26,8 +26,15 @@ public class AplikasiTodoList {
      *
      * */
     public static void viewAddTodolist(){
-
+        for (int i = 0; i < model.length; i++) {
+            String todo = model[i];
+            var nomor =  i + 1;
+            if (todo != null){
+                System.out.println(nomor + " . " + todo);
+            }
+        }
     }
+
     public static void removeTodolistt(){
 
     }
