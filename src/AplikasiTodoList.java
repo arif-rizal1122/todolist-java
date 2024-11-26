d92e2e30de85d77ebafc7c20a580910c72231b49
arif-rizal1122 <rizallarif32@gmail.com>
2024-11-26 08:59:58 +0800

    show todolist + testing

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -1,19 +1,29 @@
 
-
-
-
 public class AplikasiTodoList {
 
     public static String[] model = new String[10];
 
 
     public static void main(String[] args) {
-
+        testShowTodolist();
     }
 
     public static void showTodoList(){
-
+        for (int i = 0; i < model.length; i++) {
+            String todo = model[i];
+            var nomor =  i + 1;
+            if (todo != null){
+                System.out.println(nomor + ". " + todo);
+            }
+        }
     }
+    public static void testShowTodolist(){
+        model[0] = "basic";
+        model[1] = "java";
+        model[2] = "pzn";
+        showTodoList();
+    }
+
     public static void addTodoList() {
 
     }
@@ -26,14 +36,10 @@ public class AplikasiTodoList {
      *
      * */
     public static void viewAddTodolist(){
-        for (int i = 0; i < model.length; i++) {
-            String todo = model[i];
-            var nomor =  i + 1;
-            if (todo != null){
-                System.out.println(nomor + " . " + todo);
-            }
-        }
+
     }
+
+
 
     public static void removeTodolistt(){
 
